b59e15e506665ab68200cc97e5742ae4f6419701
cq <mrcaoqiang@163.com>
2022-02-10 22:04:27 +0800

    [rabbit-client] update and test ConnectionUtil

--- rabbitmq-client/src/main/java/com/brave/util/ConnectionUtil.java
+++ rabbitmq-client/src/main/java/com/brave/util/ConnectionUtil.java
@@ -3,33 +3,37 @@ package com.brave.util;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
-/**
- * @author: J-Brave
- * Date: 2022/1/9
- * Time: 22:58
- * Description:
- */
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class ConnectionUtil {
 
     /**
-    * @Author J-Brave
-    * @Description 获得连接对象
-    * @Date 23:02 2022/1/9
-    * @Param
-    * @return
-    **/
-    public static Connection getConnection() {
-        try {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            factory.setPort(5672);
-            factory.setUsername("brave");
-            factory.setPassword("Brave");
-            return factory.newConnection();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+     * 连接工具类
+     *
+     * @throws IOException
+     * @throws TimeoutException
+     */
+    public static Connection getConnection() throws IOException, TimeoutException {
+        //创建连接对象
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        //指定连接地址
+        connectionFactory.setHost("localhost");
+        //指定端口号
+        connectionFactory.setPort(5672);
+        //设置虚拟机 一个MQ可以设置多个虚拟机，每个虚拟机相当于一个独立的MQ
+        connectionFactory.setVirtualHost("/brave");
+        //设置账号
+        connectionFactory.setUsername("brave");
+        //设置密码
+        connectionFactory.setPassword("root");
+        //创建连接
+        return connectionFactory.newConnection();
+    }
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = getConnection();
+        System.out.println("获取连接: " + connection.toString());
     }
 
 }
